bef44e861f909c8b38617b800f400262c2a4afd4
bdf2ch <savoronov@kolenergo.ru>
2016-02-15 15:55:12 +0300

    initial commit

---
+++ dwelling.js
@@ -0,0 +1,640 @@
+"use strict";
+
+var data = {
+    "4": {
+        "id": "153813",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "4",
+        "estate_rooms": "2",
+        "geo_house_entrance": "1",
+        "estate_floor": "1",
+        "estate_area": "53.8000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "1909900.0000",
+        "estate_price_m2": "35500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "5": {
+        "id": "153814",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "5",
+        "estate_rooms": "2",
+        "geo_house_entrance": "1",
+        "estate_floor": "1",
+        "estate_area": "53.6000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "1902800.0000",
+        "estate_price_m2": "35500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView" :null
+    },
+    "10": {
+        "id": "153819",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "10",
+        "estate_rooms": "2",
+        "geo_house_entrance": "1",
+        "estate_floor": "2",
+        "estate_area": "53.8000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2017500.0000",
+        "estate_price_m2": "37500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "12": {
+        "id": "153821",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "12",
+        "estate_rooms": "2",
+        "geo_house_entrance": "1",
+        "estate_floor": "2",
+        "estate_area": "53.6000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2010000.0000",
+        "estate_price_m2": "37500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "19": {
+        "id": "153580",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "19",
+        "estate_rooms": "2",
+        "geo_house_entrance": "1",
+        "estate_floor": "3",
+        "estate_area": "53.6000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2010000.0000",
+        "estate_price_m2": "37500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "20": {
+        "id": "153581",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "20",
+        "estate_rooms": "3",
+        "geo_house_entrance": "1",
+        "estate_floor": "3",
+        "estate_area": "82.6000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2932300.0000",
+        "estate_price_m2": "35500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "26": {
+        "id": "153587",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "26",
+        "estate_rooms": "2",
+        "geo_house_entrance": "1",
+        "estate_floor": "4",
+        "estate_area": "53.6000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2010000.0000",
+        "estate_price_m2": "37500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "38": {
+        "id": "153599",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "38",
+        "estate_rooms": "2",
+        "geo_house_entrance": "1",
+        "estate_floor": "6",
+        "estate_area": "53.8000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2017500.0000",
+        "estate_price_m2": "37500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "40": {
+        "id": "153601",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "40",
+        "estate_rooms": "2",
+        "geo_house_entrance": "1",
+        "estate_floor": "6",
+        "estate_area": "53.6000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2010000.0000",
+        "estate_price_m2": "37500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "41": {
+        "id": "153602",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "41",
+        "estate_rooms": "3",
+        "geo_house_entrance": "1",
+        "estate_floor": "6",
+        "estate_area": "82.6000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2932300.0000",
+        "estate_price_m2": "35500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "45": {
+        "id": "153606",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "45",
+        "estate_rooms": "2",
+        "geo_house_entrance": "1",
+        "estate_floor": "7",
+        "estate_area": "53.8000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2017500.0000",
+        "estate_price_m2": "37500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "48": {
+        "id": "153609",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "48",
+        "estate_rooms": "3",
+        "geo_house_entrance": "1",
+        "estate_floor": "7",
+        "estate_area": "82.6000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2932300.0000",
+        "estate_price_m2": "35500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "52": {
+        "id": "153613",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "52",
+        "estate_rooms": "2",
+        "geo_house_entrance": "1",
+        "estate_floor": "8",
+        "estate_area": "53.8000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2017500.0000",
+        "estate_price_m2": "37500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "55": {
+        "id": "153616",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "55",
+        "estate_rooms": "3",
+        "geo_house_entrance": "1",
+        "estate_floor": "8",
+        "estate_area": "82.6000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2932300.0000",
+        "estate_price_m2": "35500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "59": {
+        "id": "153620",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "59",
+        "estate_rooms": "2",
+        "geo_house_entrance": "1",
+        "estate_floor": "9",
+        "estate_area": "53.5000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2006250.0000",
+        "estate_price_m2": "37500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "61": {
+        "id": "153622",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "61",
+        "estate_rooms": "2",
+        "geo_house_entrance": "1",
+        "estate_floor": "9",
+        "estate_area": "53.4000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2002500.0000",
+        "estate_price_m2": "37500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "69": {
+        "id": "153630",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "69",
+        "estate_rooms": "3",
+        "geo_house_entrance": "2",
+        "estate_floor": "2",
+        "estate_area": "82.6000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2932300.0000",
+        "estate_price_m2": "35500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "70": {
+        "id": "153631",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "70",
+        "estate_rooms": "2",
+        "geo_house_entrance": "2",
+        "estate_floor": "2",
+        "estate_area": "53.6000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2010000.0000",
+        "estate_price_m2": "37500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "77": {
+        "id": "153638",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "77",
+        "estate_rooms": "2",
+        "geo_house_entrance": "2",
+        "estate_floor": "3",
+        "estate_area": "53.6000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2010000.0000",
+        "estate_price_m2": "37500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "79": {
+        "id": "153640",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "79",
+        "estate_rooms": "2",
+        "geo_house_entrance": "2",
+        "estate_floor": "3",
+        "estate_area": "53.8000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2017500.0000",
+        "estate_price_m2": "37500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "84": {
+        "id": "153645",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "84",
+        "estate_rooms": "2",
+        "geo_house_entrance": "2",
+        "estate_floor": "4",
+        "estate_area": "53.6000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2010000.0000",
+        "estate_price_m2": "37500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "90": {
+        "id": "153651",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "90",
+        "estate_rooms": "3",
+        "geo_house_entrance": "2",
+        "estate_floor": "5",
+        "estate_area": "82.6000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2932300.0000",
+        "estate_price_m2": "35500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "104": {
+        "id": "153665",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "104",
+        "estate_rooms": "3",
+        "geo_house_entrance": "2",
+        "estate_floor": "7",
+        "estate_area": "82.6000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2932300.0000",
+        "estate_price_m2": "35500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "114": {
+        "id": "153675",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "114",
+        "estate_rooms": "2",
+        "geo_house_entrance": "2",
+        "estate_floor": "8",
+        "estate_area": "53.8000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2017500.0000",
+        "estate_price_m2": "37500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    },
+    "121": {
+        "id": "153682",
+        "status": "20",
+        "type": "living",
+        "geo_flatnum": "121",
+        "estate_rooms": "2",
+        "geo_house_entrance": "2",
+        "estate_floor": "9",
+        "estate_area": "53.5000",
+        "estate_area_living": null,
+        "estate_area_kitchen": null,
+        "estate_area_loggia": null,
+        "estate_price": "2006250.0000",
+        "estate_price_m2": "37500.0000",
+        "estate_living_new": "1",
+        "estate_category_type": "12285",
+        "estate_windowView": null
+    }
+};
+
+
+
+
+
+
+
+
+
+
+function House () {
+    this.number = 0;
+    this.img = "";
+};
+
+
+
+function Flat () {
+    this.id = 0;
+    this.status = 0;
+    this.type = "";
+    this.flatNumber = 0;
+    this.roomsCount = 0;
+    this.entrance = 0;
+    this.floor = 0;
+    this.area = {
+        total: 0,
+        living: 0,
+        kitchen: 0,
+        loggia: 0
+    };
+    this.price = 0;
+    this.pricePerMeter = 0;
+    this.livingNew = 0;
+    this.categoryType = 0;
+    this.windowView = 0;
+};
+
+
+
+
+
+var dwellingModule = angular.module("dwelling", [])
+    .config(function ($provide) {
+        $provide.service("$dwelling", ["$log", "$http", function ($log, $http) {
+            var service = {};
+
+            var container = document.getElementById("dwelling-svg");
+            var complex = undefined;
+            var queues = [];
+
+
+            /**
+             * Класс, описывающий жилой комплекс
+             * @param parameters {Object} - параметры инициализации объекта
+             * @constructor
+             */
+            function Complex (parameters) {
+                this.img = "";
+                var queues = [];
+
+                if (parameters !== undefined) {
+                    for (var param in parameters) {
+                        if (this.hasOwnProperty(param))
+                            this[param] = parameters[param];
+                    }
+                    if (this.img !== undefined && this.img !== "") {
+                        container.style.backgroundImage = "url('" + this.img + "')";
+                    }
+                }
+
+                this.queues = {
+                    get: function () {
+                        return queues;
+                    },
+                    add: function (parameters) {
+                        if (parameters !== undefined) {
+                            var queue = new Queue(parameters);
+                            queues.push(queue);
+                        }
+                    }
+                };
+            };
+
+
+
+            /**
+             * Класс, описывающий очередь строительства
+             * @param parameters {Object} - параметры инициализации объекта
+             * @constructor
+             */
+            function Queue (parameters) {
+                this.img = "";
+                this.geometry = [];
+
+                if (parameters !== undefined) {
+                    for (var param in parameters) {
+                        if (this.hasOwnProperty(param))
+                            this[param] = parameters[param];
+                    }
+                    if (this.geometry.constructor === Array && this.geometry.length > 0) {
+                        var length = this.geometry.length;
+                        var points = "";
+                        for (var i = 0; i < length; i++) {
+                            var points = points +  this.geometry[i][0] + "," + this.geometry[i][1];
+                            points = (i < length - 1) ? points + "," : points;
+                        }
+                        var polyline = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
+                        polyline.setAttribute('points', points);
+                        container.
+                        container.appendChild(polyline);
+                    }
+                }
+
+                this.draw = function () {
+                    if (geometry.length > 0) {
+
+                    }
+                };
+            };
+
+
+
+            service.complex = {
+                set: function (parameters) {
+                    if (parameters !== undefined) {
+                        complex = new Complex(parameters);
+                    }
+                },
+
+                get: function () {
+                    return complex;
+                }
+            };
+
+
+            service.getQueue = function () {
+                $http.post("")
+                    .success(function (data) {
+                        if (data !== undefined) {
+
+                        }
+                    });
+            };
+
+
+            service.getHouses = function () {
+                $http.post("")
+                    .success(function (data) {
+                        if (data !== undefined) {
+
+                        }
+                    });
+            };
+
+
+            service.getFlats = function (houseId) {
+                $http.post("")
+                    .success(function (data) {
+                        if (data !== undefined) {
+
+                        }
+                    });
+            };
+
+
+            return service;
+        }])
+    })
+    .run(function ($log, $dwelling) {
+        $dwelling.complex.set({
+            img: "img/complex.jpg"
+        });
+        $dwelling.complex.get().queues.add({
+            geometry: [[100, 100], [50, 300], [350, 300], [400, 100]]
+        });
+
+        $dwelling.complex.get().queues.add({
+            geometry: [[500, 300], [500, 400], [800, 400], [800, 300]]
+        });
+
+        $log.log("complex = ", $dwelling.complex.get());
+    });
+
+
+var dwellingApp = angular.module("dwellingApp", ["dwelling"]);
